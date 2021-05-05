1bbda2269106b390907e518fdee584072019b95a
Chillig <61054943+Chillig@users.noreply.github.com>
2021-05-05 15:21:12 +0200

    plots are now saved to .pdf file in function

--- r_scripts/pathway_analysis/plot_pathways.R
+++ r_scripts/pathway_analysis/plot_pathways.R
@@ -9,9 +9,10 @@ library(cowplot)
 ################################ --------> plots <---------- ###########################
 # Source: https://yulab-smu.github.io/clusterProfiler-book/chapter12.html
 # Gene Concept Network
-fig.pathways.REACTOME <- function(reactome_res, entrezid_log2fc, showCategories) 
+fig.pathways.REACTOME <- function(reactome_res, entrezid_log2fc, showCategories, title,
+                                  width, height, output.dir) 
 {
-  if (type(showCategories) != 'character') 
+  if (typeof(showCategories) != 'character') 
   {
     if (showCategories > nrow(reactome_res)) 
     {
@@ -22,61 +23,57 @@ fig.pathways.REACTOME <- function(reactome_res, entrezid_log2fc, showCategories)
     # get intersection Pathways which are in enrichObject and in the user provided Pathway list
     showCategories = intersect(reactome_res$Description, showCategories)
   }
+  
+  if (all(entrezid_log2fc) < 0) 
+  {
+    low = 'red'
+    mid = 'red'
+    high = 'blue'
+  } else {
+    low = 'blue'
+    mid = 'blue'
+    high = 'red'
+  }
 
-  par(mfrow = c(1, 1))
+  pdf(file = file.path(output.dir, title), width = width, height = height)
   p1 = enrichplot::cnetplot(reactome_res, showCategory = showCategories, 
                             categorySize = "pvalue", foldChange = entrezid_log2fc, 
-                            colorEdge = TRUE) + ggtitle(
-                              "REACTOME: Pathway Enrichment Analysis using DB Reactome") +
-    scale_colour_gradient2(name = "fold change", low = "green", mid = "white", high = "red")
+                            colorEdge = TRUE) + 
+    ggtitle("REACTOME: Pathway Enrichment Analysis using DB Reactome") 
+  # Color upper and lower border
+  min.value <- floor( min(p1$data$color, na.rm = TRUE) )
+  max.value <- ceiling( max(p1$data$color, na.rm = TRUE) )
+
+  p1 <- p1 + scale_color_gradientn(
+    name = "fold change", colours = c("blue", "red"), limits= c(min.value, max.value))
   cowplot::plot_grid(p1, ncol = 1, nrow = 1)
+  print(p1)
+  dev.off()
+  
+  p1 
 }
+
 
 #' Plot pathways in a dotplot
 #' 
 #' @param pathway_res : enrichobject
 #' @param showCategories : str, int
 #'   which or how many pathways to visualise; limited by max. number of enriched pathways
-fig.pathway.dotplot <- function(pathway_res, showCategories, method) 
+fig.pathway.dotplot <- function(pathway_res, showCategories, method, title,
+                                width, height, output.dir) 
 {
-  if (type(showCategories) != 'character') 
+  if (typeof(showCategories) != 'character') 
   {
     if (showCategories > nrow(pathway_res)) 
     {
       showCategories = nrow(pathway_res)
     }
   } 
+  
+  pdf(file = file.path(output.dir, title), width = width, height = height)
   p1 = enrichplot::dotplot(pathway_res, showCategory = showCategories, color='p.adjust') + 
     ggtitle(paste(method, ": Pathway Enrichment Analysis", sep = " "))
-  
   cowplot::plot_grid(p1, ncol = 1, nrow = 1)
+  print(p1)
+  dev.off()
 }
-
-#' Visualise pathways in a network
-#' 
-#' @param pathway_res : enrichobject
-#' @param genelist_log2FC : list of log2FC values received from DGE analysis
-#' @param nodelabel choose between: category, all, gene, none
-fig.cnetplot <- function(pathway_res, genelist_log2FC, nodelabel) 
-{
-  p1 = enrichplot::cnetplot(pathway_res, categorySize = "pvalue", foldChange = genelist_log2FC, 
-                            circular = TRUE, colorEdge = TRUE, node_label = nodelabel)
-  cowplot::plot_grid(p1, ncol = 1, nrow = 1)
-  
-}
-
-#' Plot pathways in a dotplot
-#' 
-#' @param pathway_res : enrichobject
-#' @param showCategories : int
-#'   how many pathways to visualise; limited by max. number of enriched pathways
-fig.dotplot <- function(pathway_res, showCategories) 
-{
-  if (showCategories > nrow(pathway_res)) 
-  {
-    showCategories = nrow(pathway_res)
-  }
-  p1 = enrichplot::dotplot(pathway_res, showCategory = showCategories) 
-  cowplot::plot_grid(p1, ncol = 1, nrow = 1)
-}
-
