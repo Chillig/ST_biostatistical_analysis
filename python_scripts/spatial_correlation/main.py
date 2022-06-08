6144af38b1d4af60d2951fbf89790187ae663229
Chillig <61054943+Chillig@users.noreply.github.com>
2022-06-08 15:23:40 +0200

    python_scripts/spatial_correlation/main.py: adjusted for new data and made more flexible

--- python_scripts/spatial_correlation/main.py
+++ python_scripts/spatial_correlation/main.py
@@ -9,20 +9,27 @@
 """
 
 from python_scripts.spatial_correlation import density_clustering
+from python_scripts.utils import gene_lists
 
 import os
 from datetime import date
 import scanpy as sc
+import numpy as np
 
 
-def main(save_folder, adata):
+def main(save_folder: str, adata, radius: [int, list], cond_genes: list, genes_resps: dict,
+         find_responders=False, corr_method='pearson', get_plots=False):
     # parameter
     tissue_layers = ['upper EPIDERMIS', 'middle EPIDERMIS', 'basal EPIDERMIS']
-    radius = 1  # or list [1, 2, 3, ..]
-    get_plots = False
+    epidermis_layers = ['upper EPIDERMIS', 'middle EPIDERMIS', 'basal EPIDERMIS']
+    # radius = list(np.arange(0, 6, 1))  # 1 or list [1, 2, 3, ..]
 
-    density_clustering.main(adata=adata, save_folder=save_folder, tissue_types=tissue_layers,
-                            radii=radius, get_plots=get_plots)
+    counts_dict = density_clustering.main(
+        adata=adata, save_folder=save_folder, tissue_types=tissue_layers, epidermis_layers=epidermis_layers,
+        radii=radius, get_plots=get_plots, corr_method=corr_method, conditional_genes=cond_genes,
+        conditionalgenes_responders=genes_resps, find_responders=find_responders)
+
+    return counts_dict
 
 
 if __name__ == '__main__':
@@ -30,10 +37,15 @@ if __name__ == '__main__':
     # replace os.environ['PYTHONPATH'].split(os.pathsep)[0] with sys.path[2] -> can be run then in terminal
     path = os.path.join("..", "..")
     # create saving folder in current project path
-    savepath = os.path.join(path, "output", "Fig4C_Spatial_weighted_correlation", str(today))
+    savepath = os.path.join(path, "output", "Fig5F-H_Spatial_weighted_correlation", str(today))
     os.makedirs(savepath, exist_ok=True)
 
     # Load Raw anndata --> used for publication figure 4
-    unpp_st_adata = sc.read(os.path.join(path, "adata_storage", "2020-10-06", "st_adata_P15509_P16357_wo_4_7_unpp.h5"))
+    unpp_st_adata = sc.read(
+        os.path.join(path, "adata_storage", "2022-04-08", "Spatial Transcriptomics_unpp_cleaned_PsoADLP.h5"))
 
-    main(save_folder=savepath, adata=unpp_st_adata)
+    # 1. Get cytokines and responders
+    conditional_genes, _, conditionalgenes_responders = gene_lists.get_publication_cyto_resps()
+
+    main(save_folder=savepath, adata=unpp_st_adata, cond_genes=conditional_genes,
+         genes_resps=conditionalgenes_responders, radius=1)
