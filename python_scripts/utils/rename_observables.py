c5be7a037545f319c66199cd5cf596fcf3dec01f
Chillig <61054943+Chillig@users.noreply.github.com>
2022-11-09 07:56:40 +0100

    adjusted function to work on new data

--- python_scripts/utils/rename_observables.py
+++ python_scripts/utils/rename_observables.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
 
-def interface_to_epidermis(adata, tissue_layers):
+def interface_to_epidermis(adata, tissue_layers_obsname, skin_layers):
     # Rename tissue region 'INTERFACE' to upper, middle or basal EPIDERMIS because some spots got both labels
-    m_interface = adata.obs['tissue_type'] == 'INTERFACE'
-    if isinstance(tissue_layers, list):
-        df_temp = adata.obs[tissue_layers][m_interface]
+    m_interface = adata.obs['JUNCTION'] == 1
+    if isinstance(skin_layers, list):
+        df_temp = adata.obs[skin_layers][m_interface]
     else:
-        df_temp = adata.obs[[tissue_layers]][m_interface]
+        df_temp = adata.obs[[skin_layers]][m_interface]
     df_temp = df_temp.loc[:, df_temp.columns].replace(1, pd.Series(df_temp.columns, df_temp.columns))
-    df_temp['interface'] = '0'
+    df_temp['JUNCTION'] = '0'
     for col in df_temp.columns[:-1]:
-        df_temp['interface'] += df_temp[col].astype(str)
-    df_temp["interface"] = df_temp.interface.str.replace('0', '')
-    adata.obs['tissue_type'][m_interface] = df_temp["interface"]
-    adata.obs['tissue_type'] = adata.obs['tissue_type'].cat.remove_unused_categories()
+        df_temp['JUNCTION'] += df_temp[col].astype(str)
+    df_temp["JUNCTION"] = df_temp.JUNCTION.str.replace('0', '')
+    adata.obs[tissue_layers_obsname][m_interface] = df_temp["JUNCTION"]
+    adata.obs[tissue_layers_obsname] = adata.obs[tissue_layers_obsname].cat.remove_unused_categories()
 
     return adata
